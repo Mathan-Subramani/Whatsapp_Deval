06ecc7a61ce87e02883f6ba7320b18065479d5cc
Mathan-Subramani <97078859+Mathan-Subramani@users.noreply.github.com>
2022-01-26 09:45:08 +0530

    Update FirstSample.java

--- FirstSample.java
+++ FirstSample.java
@@ -4,6 +4,6 @@ System.out.println("Hey Mathan");
 System.out.println("Hey Karthi");
 System.out.println("Hey Surya");
 System.out.println("Hey Vikram");
-System.out.println("Am Dhoni and Virat");
+System.out.println("Am Dhoni");
 }
 }
